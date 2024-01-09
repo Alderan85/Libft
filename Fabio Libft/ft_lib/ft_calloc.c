8fedd73d74ac95cff3311d88d950f3fb2778ec0a
nencinif <62701030+nencinif@users.noreply.github.com>
2024-01-09 17:23:16 +0100

    Add files via upload
    calloc

---
+++ Fabio Libft/ft_lib/ft_calloc.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fane <fane@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 17:04:03 by fane              #+#    #+#             */
+/*   Updated: 2024/01/09 17:16:04 by fane             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+void    *ft_calloc(size_t count, size_t size)
+{
+    unsigned char *tmp;
+    size_t i;
+    size_t total;
+    total = count * size;
+    i = 0;
+    
+
+    tmp = malloc(total);
+    if (!tmp)
+        return(NULL);
+
+    while (i < total)
+    {
+        tmp[i] = 0;
+        i++;
+    }
+    return(tmp);
+}
+
+/*
+int main()
+{
+    int *arr = ft_calloc(5, sizeof(int));
+    if (arr == NULL)
+    {
+        printf("errore di allocazione.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
+*/
