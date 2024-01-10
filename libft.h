291adb4524a236841679c847dad06ed383a6c50a
Alderan85 <140227101+Alderan85@users.noreply.github.com>
2024-01-10 17:46:30 +0100

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabnenci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 17:22:12 by fane              #+#    #+#             */
+/*   Updated: 2023/11/18 17:23:49 by fane             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H	
+# define LIBFT_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int		ft_atoi( const char *str);
+int		ft_strlen(const char *str);
+int		ft_isalnum(int i);
+int		ft_isalpha(int i);
+int		ft_isascii(int i);
+int		ft_isdigit(int i);
+int		ft_isprint(int i);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putendl_fd(char *s,int fd);
+void		ft_putstr_fd(char *src, int fd);
+char		*ft_strcpy(char *dest, const char *src);
+#endif
