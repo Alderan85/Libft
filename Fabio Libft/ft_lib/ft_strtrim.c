ef5a87383fd60fd9790588352170f86654c9b2bf
nencinif <62701030+nencinif@users.noreply.github.com>
2024-01-11 23:01:12 +0100

    strchr strtrim

--- Fabio Libft/ft_lib/ft_strtrim.c
+++ Fabio Libft/ft_lib/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fabnenci <fabnenci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fane <fane@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:35:04 by fabnenci          #+#    #+#             */
-/*   Updated: 2024/01/10 17:38:41 by fabnenci         ###   ########.fr       */
+/*   Updated: 2024/01/11 19:11:23 by fane             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	charcheck(const char *set, char a)
 	int	i;
 
 	i = 0;
+
 	while ( set[i] != '\0' )
 	{
-		if (set[i] == a)
-			{
-				return (1);
-			}
+		if(set[i] == a)
+			return(1);
+		i++;
 	}
 	return (0);
 }
@@ -31,14 +31,41 @@ char	*ft_strtrim(const char *s1, const char *set)
 {
 	int	i;
 	int	y;
+	int	len;
+	char *trm;
 
+	len = ft_strlen(s1);
 	i = 0;
 	y = 0;
-if(!set)
-	return(ft_strdup());
-
-	while(charcheck(set, s1[i]))
+	while (i < len && charcheck(set, s1[i]))
 	{
 		i++;
+
 	}
+
+	while (len > i && charcheck(set, s1[len - 1] ))
+	{
+		len--;
+	}
+
+	trm = malloc(sizeof(char) * (len - i + 1));
+
+	while(i < len)
+	{
+		trm[y++] = s1[i++];
+	}
+
+	trm[y] = '\0';
+
+	return(trm);
+
 }
+/*
+int	main()
+{
+	char	uno[7] = "banana";
+	char	due[3] = "ba";
+
+	printf("%s",ft_strtrim(uno, due));
+}
+*/
