56c558c9b0981a1ebb58d9d97f0d5c7ca6bfe23f
nencinif <62701030+nencinif@users.noreply.github.com>
2024-01-19 16:30:17 +0100

    strrchr

---
+++ Fabio Libft/ft_lib/ft_strrchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fabnenci <fabnenci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 15:43:57 by fabnenci          #+#    #+#             */
+/*   Updated: 2024/01/19 16:24:54 by fabnenci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+
+	while (len >= 0)
+	{
+		if (s[len] == c)
+			return ((char *)&s[len]);
+		len--;
+	}
+	return (NULL);
+}
+/*
+int main()
+{
+	char s[] = "afragolacci";
+	int c = 'a';
+	printf("%s\n", ft_strrchr(s, c));
+	//write (1, s, 6);
+	write (1, "\n", 1);
+}
+*/
