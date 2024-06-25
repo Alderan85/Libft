f07c03798f482d65bc4deea2eb8d680ce891b741
Alderan85 <140227101+Alderan85@users.noreply.github.com>
2024-06-25 13:12:12 +0200

    Delete Fabio Libft directory

--- Fabio Libft/ft_lib/prova.c
+++
@@ -1,8 +0,0 @@
-# include "libft.h"
-
-int main()
-{
-	char source[7] = "banana";
-	
-	printf("%d",ft_strlen(source));
-}
--- Fabio Libft/ft_lib/prova2.c
+++
@@ -1,8 +0,0 @@
-//#include <unistd.h>
-//#include <libft.h>
-#include "libft.h"
-int	main()
-{
-	char *s = " ---+--+1234ab567";
-	printf("%d\n", ft_atoi(s));
-}
